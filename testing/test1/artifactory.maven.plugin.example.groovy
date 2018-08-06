2a9dfd9b990566031f9b0d4357802e3337dcb832
Alexey Andreev <alexankk@gmail.com>
2018-08-06 04:39:51 +0300

    New access parameters

--- testing/test1/artifactory.maven.plugin.example.groovy
+++ testing/test1/artifactory.maven.plugin.example.groovy
@@ -81,55 +81,60 @@ EOF
         stage('Deploy artifactory'){
             steps{
                 script{
-                    sh('''
-                        set +x
-                        cd linux-amd64
-                        ./helm install --name artifactory \\
-                       --set artifactory.image.repository=docker.bintray.io/jfrog/artifactory-oss \\
-                       --set artifactory.resources.requests.cpu="500m" \\
-                       --set artifactory.resources.limits.cpu="2" \\
-                       --set artifactory.resources.requests.memory="1Gi" \\
-                       --set artifactory.resources.limits.memory="2Gi" \\
-                       --set artifactory.javaOpts.xms="1g" \\
-                       --set artifactory.javaOpts.xmx="2g" \\
-                       --set nginx.resources.requests.cpu="100m" \\
-                       --set nginx.resources.limits.cpu="250m" \\
-                       --set nginx.resources.requests.memory="250Mi" \\
-                       --set nginx.resources.limits.memory="500Mi" \\
-                       stable/artifactory
-                    ''')
+                    artDeployed=(sh(script:'./helm ls --all artifactory | grep artifactory | sed "s/.*\\(DEPLOYED\\).*/\\1/"',returnStdout: true).trim()=='DEPLOYED')
+                    if (!artDeployed){
+                        sh('''
+                            set +x
+                            cd linux-amd64
+                            ./helm install --name artifactory \\
+                           --set artifactory.image.repository=docker.bintray.io/jfrog/artifactory-oss \\
+                           --set artifactory.resources.requests.cpu="500m" \\
+                           --set artifactory.resources.limits.cpu="2" \\
+                           --set artifactory.resources.requests.memory="1Gi" \\
+                           --set artifactory.resources.limits.memory="2Gi" \\
+                           --set artifactory.javaOpts.xms="1g" \\
+                           --set artifactory.javaOpts.xmx="2g" \\
+                           --set nginx.resources.requests.cpu="100m" \\
+                           --set nginx.resources.limits.cpu="250m" \\
+                           --set nginx.resources.requests.memory="250Mi" \\
+                           --set nginx.resources.limits.memory="500Mi" \\
+                           stable/artifactory
+                        ''')
+                    }
                 }
             }
         }
         stage('Waiting for services are redy'){
             steps{
                 script{
-                    sh('''
-                        set +x
-                        echo "Waiting for services are redy"
-                        ti=90
-                        while [ $ti -gt 0 ]; do
-                            sleep 10
-                            ART_STATE=$(kubectl get po | grep -v nginx | grep artifactory-artifactory | sed "s/.*\\(Running\\).*/\\1/")
-                            if [ "X${ART_STATE}" = "XRunning" ]; then ti=0; fi
-                            ti=$(($ti-1))
-                            if [ $ti -eq 0 ]; then 
-                                echo "Timeout starting Artifactory service in cluster"
-                                exit 1
-                            fi
-                        done
-                        ti=90
-                        while [ $ti -gt 0 ]; do
-                            sleep 10
-                            NGX_STATE=$(kubectl get po | grep nginx | sed "s/.*\\(Running\\).*/\\1/")
-                            if [ "X${NGX_STATE}" = "XRunning" ]; then ti=0; fi
-                            ti=$(($ti-1))
-                            if [ $ti -eq 0 ]; then 
-                                echo "Timeout starting Artifactory-Nginx service in cluster"
-                                exit 1
-                            fi
-                        done
-                    ''')
+                    if (!artDeployed){
+                        sh('''
+                            set +x
+                            echo "Waiting for services are redy"
+                            ti=90
+                            while [ $ti -gt 0 ]; do
+                                sleep 10
+                                ART_STATE=$(kubectl get po | grep -v nginx | grep artifactory-artifactory | sed "s/.*\\(Running\\).*/\\1/")
+                                if [ "X${ART_STATE}" = "XRunning" ]; then ti=0; fi
+                                ti=$(($ti-1))
+                                if [ $ti -eq 0 ]; then 
+                                    echo "Timeout starting Artifactory service in cluster"
+                                    exit 1
+                                fi
+                            done
+                            ti=90
+                            while [ $ti -gt 0 ]; do
+                                sleep 10
+                                NGX_STATE=$(kubectl get po | grep nginx | sed "s/.*\\(Running\\).*/\\1/")
+                                if [ "X${NGX_STATE}" = "XRunning" ]; then ti=0; fi
+                                ti=$(($ti-1))
+                                if [ $ti -eq 0 ]; then 
+                                    echo "Timeout starting Artifactory-Nginx service in cluster"
+                                    exit 1
+                                fi
+                            done
+                        ''')
+                    }
                     haExtIp=sh(script:'kubectl get svc --namespace default artifactory-artifactory-nginx -o jsonpath=\'{.status.loadBalancer.ingress[0].ip}\'',returnStdout: true).trim()
                     echo 'External IP for balancer: '+haExtIp
                 }
@@ -138,24 +143,26 @@ EOF
         stage('Creating repositories'){
             steps{
                 script{
-                    sh('''
-                        set +x
-                        ART_AUTH=$(echo -n "'''+artUser+''':'''+artPass+'''" | base64)
-                        curl -s --header "Content-Type: application/json" \\
-                        --header "Authorization:Basic ${ART_AUTH}" \\
-                        --request POST \\
-                        --data \'{"type":"localRepoConfig","typeSpecific":{"localChecksumPolicy":"CLIENT","repoType":"Maven","icon":"maven","text":"Maven","maxUniqueSnapshots":"","handleReleases":true,"handleSnapshots":true,"suppressPomConsistencyChecks":false,"snapshotVersionBehavior":"UNIQUE","eagerlyFetchJars":false,"eagerlyFetchSources":false,"remoteChecksumPolicy":"GEN_IF_ABSENT","listRemoteFolderItems":true,"rejectInvalidJars":false,"pomCleanupPolicy":"discard_active_reference","url":"https://jcenter.bintray.com"},"advanced":{"cache":{"keepUnusedArtifactsHours":"","retrievalCachePeriodSecs":600,"assumedOfflineLimitSecs":300,"missedRetrievalCachePeriodSecs":1800},"network":{"socketTimeout":15000,"syncProperties":false,"lenientHostAuth":false,"cookieManagement":false},"blackedOut":false,"allowContentBrowsing":false},"basic":{"includesPattern":"**/*","includesPatternArray":["**/*"],"excludesPatternArray":[],"layout":"maven-2-default","publicDescription":"maven repo","internalDescription":"maven repo in"},"general":{"repoKey":"libs-release-local"}}\' \\
-                        http://'''+haExtIp+'''/artifactory/ui/admin/repositories
-                    ''')
-                    sh('''
-                        set +x
-                        ART_AUTH=$(echo -n "'''+artUser+''':'''+artPass+'''" | base64)
-                        curl -s --header "Content-Type: application/json" \\
-                        --header "Authorization:Basic ${ART_AUTH}" \\
-                        --request POST \\
-                        --data \'{"type":"localRepoConfig","typeSpecific":{"localChecksumPolicy":"CLIENT","repoType":"Maven","icon":"maven","text":"Maven","maxUniqueSnapshots":"","handleReleases":true,"handleSnapshots":true,"suppressPomConsistencyChecks":false,"snapshotVersionBehavior":"UNIQUE","eagerlyFetchJars":false,"eagerlyFetchSources":false,"remoteChecksumPolicy":"GEN_IF_ABSENT","listRemoteFolderItems":true,"rejectInvalidJars":false,"pomCleanupPolicy":"discard_active_reference","url":"https://jcenter.bintray.com"},"advanced":{"cache":{"keepUnusedArtifactsHours":"","retrievalCachePeriodSecs":600,"assumedOfflineLimitSecs":300,"missedRetrievalCachePeriodSecs":1800},"network":{"socketTimeout":15000,"syncProperties":false,"lenientHostAuth":false,"cookieManagement":false},"blackedOut":false,"allowContentBrowsing":false},"basic":{"includesPattern":"**/*","includesPatternArray":["**/*"],"excludesPatternArray":[],"layout":"maven-2-default","publicDescription":"maven repo","internalDescription":"maven repo in"},"general":{"repoKey":"libs-snapshot-local"}}\' \\
-                        http://'''+haExtIp+'''/artifactory/ui/admin/repositories
-                    ''')
+                    if (!artDeployed){
+                        sh('''
+                            set +x
+                            ART_AUTH=$(echo -n "'''+artUser+''':'''+artPass+'''" | base64)
+                            curl -s --header "Content-Type: application/json" \\
+                            --header "Authorization:Basic ${ART_AUTH}" \\
+                            --request POST \\
+                            --data \'{"type":"localRepoConfig","typeSpecific":{"localChecksumPolicy":"CLIENT","repoType":"Maven","icon":"maven","text":"Maven","maxUniqueSnapshots":"","handleReleases":true,"handleSnapshots":true,"suppressPomConsistencyChecks":false,"snapshotVersionBehavior":"UNIQUE","eagerlyFetchJars":false,"eagerlyFetchSources":false,"remoteChecksumPolicy":"GEN_IF_ABSENT","listRemoteFolderItems":true,"rejectInvalidJars":false,"pomCleanupPolicy":"discard_active_reference","url":"https://jcenter.bintray.com"},"advanced":{"cache":{"keepUnusedArtifactsHours":"","retrievalCachePeriodSecs":600,"assumedOfflineLimitSecs":300,"missedRetrievalCachePeriodSecs":1800},"network":{"socketTimeout":15000,"syncProperties":false,"lenientHostAuth":false,"cookieManagement":false},"blackedOut":false,"allowContentBrowsing":false},"basic":{"includesPattern":"**/*","includesPatternArray":["**/*"],"excludesPatternArray":[],"layout":"maven-2-default","publicDescription":"maven repo","internalDescription":"maven repo in"},"general":{"repoKey":"libs-release-local"}}\' \\
+                            http://'''+haExtIp+'''/artifactory/ui/admin/repositories
+                        ''')
+                        sh('''
+                            set +x
+                            ART_AUTH=$(echo -n "'''+artUser+''':'''+artPass+'''" | base64)
+                            curl -s --header "Content-Type: application/json" \\
+                            --header "Authorization:Basic ${ART_AUTH}" \\
+                            --request POST \\
+                            --data \'{"type":"localRepoConfig","typeSpecific":{"localChecksumPolicy":"CLIENT","repoType":"Maven","icon":"maven","text":"Maven","maxUniqueSnapshots":"","handleReleases":true,"handleSnapshots":true,"suppressPomConsistencyChecks":false,"snapshotVersionBehavior":"UNIQUE","eagerlyFetchJars":false,"eagerlyFetchSources":false,"remoteChecksumPolicy":"GEN_IF_ABSENT","listRemoteFolderItems":true,"rejectInvalidJars":false,"pomCleanupPolicy":"discard_active_reference","url":"https://jcenter.bintray.com"},"advanced":{"cache":{"keepUnusedArtifactsHours":"","retrievalCachePeriodSecs":600,"assumedOfflineLimitSecs":300,"missedRetrievalCachePeriodSecs":1800},"network":{"socketTimeout":15000,"syncProperties":false,"lenientHostAuth":false,"cookieManagement":false},"blackedOut":false,"allowContentBrowsing":false},"basic":{"includesPattern":"**/*","includesPatternArray":["**/*"],"excludesPatternArray":[],"layout":"maven-2-default","publicDescription":"maven repo","internalDescription":"maven repo in"},"general":{"repoKey":"libs-snapshot-local"}}\' \\
+                            http://'''+haExtIp+'''/artifactory/ui/admin/repositories
+                        ''')
+                    }
                 }
             }
         }
