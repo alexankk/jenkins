6a292ee3113d333f0d0441154a88c05da9a7b71d
Alexey Andreev <alexankk@gmail.com>
2018-08-06 23:34:23 +0300

    New flow

--- testing/test1/artifactory.maven.plugin.example.groovy
+++ testing/test1/artifactory.maven.plugin.example.groovy
@@ -22,7 +22,7 @@ pipeline{
                 script{
                     sh('''
                         mkdir -p $WORKSPACE/bin
-                        HELM_VER=$(curl -sL https://github.com/kubernetes/helm/releases | sed -n \'/Latest release<\\/a>/,$p\' | grep -oE \\'v[0-9]+\\.[0-9]+\\.[0-9]+\' |head -1)
+                        HELM_VER=$(curl -sL https://github.com/kubernetes/helm/releases | sed -n \'/Latest release<\\/a>/,$p\' | grep -oE \\\'v[0-9]+\\.[0-9]+\\.[0-9]+\' |head -1)
                         curl -sLO https://storage.googleapis.com/kubernetes-helm/helm-$HELM_VER-linux-amd64.tar.gz
                         mv -f $(tar -xzvf helm-$HELM_VER-linux-amd64.tar.gz | grep helm) $WORKSPACE/bin
                         curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
